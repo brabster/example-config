69af5fbaa2187ca933ce233c287f882340d229ba
Paul Brabban <paul.brabban@gmail.com>
2016-06-08 12:47:36 +0100

    fix setup issues and add projectile, solarized theme

--- lisp/cfg-flycheck.el
+++ lisp/cfg-flycheck.el
@@ -1,11 +1,12 @@
-;; manual dependencies
-(add-to-list 'load-path (concat user-emacs-directory "site-lisp/" "flycheck-20150207.329"))
-
 (require-package 'let-alist)
 (require-package 'flycheck-clojure)
+(require-package 'flycheck-pos-tip)
 
 (require 'flycheck)
 
 (eval-after-load 'flycheck '(flycheck-clojure-setup))
 
 (add-hook 'after-init-hook #'global-flycheck-mode)
+
+(eval-after-load 'flycheck
+  '(setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages))
---
+++ lisp/user-customizations.el
@@ -0,0 +1,9 @@
+;;; projectile setup
+(require-package 'projectile)
+(projectile-global-mode)
+
+;;; theme setup
+(require-package 'solarized-theme)
+(load-theme 'solarized-dark t)
+(setq x-underline-at-descent-line t)
+
