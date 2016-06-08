91a0c6723ac23c453adbc48ed6d26b9e59d919bf
Paul Brabban <paul.brabban@gmail.com>
2016-06-08 15:16:51 +0100

    disable autosave and backup
    we use git for anything that matters

--- lisp/user-customizations.el
+++ lisp/user-customizations.el
@@ -71,3 +71,7 @@
 (projectile-global-mode)
 (setq projectile-require-project-root nil)
 (setq projectile-switch-project-action 'projectile-dired)
+
+;;; disable autosave and backup
+(setq auto-save-default nil)
+(setq backup-inhibited t)
